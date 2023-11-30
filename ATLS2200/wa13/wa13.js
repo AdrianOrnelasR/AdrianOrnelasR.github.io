2af8d715efdfe8149fb7b685f1c78d57a6fccbaf
Adrian Ornelas <Ador2698@colorado.edu>
2023-11-30 12:00:30 -0700

    Update wa13.js

--- ATLS2200/wa13/wa13.js
+++ ATLS2200/wa13/wa13.js
@@ -29,12 +29,15 @@ let company = {
 }
 
 // Question 1
+console.log("***********QUESTION 1***********");
 console.log(HR["employees"]);
 
 // Question 2
+console.log("***********QUESTION 2***********");
 console.log(company["companyInfo"]);
 
 // Question 3
+console.log("***********QUESTION 3***********");
 HR["employees"].push({
     "name": "Anna",
     "department": "Tech",
@@ -42,29 +45,42 @@ HR["employees"].push({
     "salary": 25600,
     "raise eligible": false
     });
-console.log(HR["employees"][3]);
+console.log(" - ", HR["employees"][3]);
 
 // Question 4
+console.log("***********QUESTION 4***********");
 let total = 0;
 for(const val of HR["employees"]){
     total += val.salary;
 }
-console.log("Total salary for all company employees: ", total);
+console.log(" - ", "Total salary for all company employees: ", total);
 
 // Question 5
+console.log("***********QUESTION 5***********");
+function raiseTime(sal){
+    sal = sal * 1.10;
+    return sal;
+}
+
 for(const em of HR["employees"]){
     if(em["raise eligible"] == true){
-        em.salary = em.salary * 1.10;
+        em.salary = raiseTime(em.salary);
+        em["raise eligible"] = false;
+        console.log(" - ", em.name, " got a raise and now earns: ", em.salary);
+    }else{
+        console.log(" - ", em.name, " did not get a raise and still earns: ", em.salary);
     }
 }
-console.log(HR["employees"]);
+
 
 // Question 6
+console.log("***********QUESTION 6***********");
 for(const em of HR["employees"]){
     if(em.name == "Anna" || em.name == "Sam"){
         em.wfh = true;
+        console.log(" - ", em.name, " works from home!")
     }else{
         em.wfh = false;
+        console.log(" - ", em.name, " dose not work from home");
     }
 }
-console.log(HR["employees"]);
